1fb65be04a46a98434cd98ac9b484a51b8ea677b
Revan0907 <99360000+Revan0907@users.noreply.github.com>
2023-05-06 11:15:27 +0100

    Delete Pandora directory

--- Pandora/README.md
+++
@@ -1,3 +0,0 @@
-Flask, HTML, CSS, Python3.9, Jinja   - Machine Learning Mental Health Chatbot
-
-This is the release version of my website.
--- Pandora/website/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
-
-class MessageText(FlaskForm):
-    message = StringField('Message', validators=[DataRequired()])
-    submit = SubmitField('Send')
